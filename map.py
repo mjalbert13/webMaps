dada2050c0b4462f0dccb6122b6c6dae7bca8f5f
mjalbert13 <mjalbert13@gmail.com>
2020-02-04 14:09:32 -0500

    adding data and markers

--- map.py
+++ map.py
@@ -1,9 +1,17 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles= "Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.1,-99.1], [37.4, -98.9]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="hello world", icon=folium.Icon(color="green")))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(str(el+" m"),parse_html=True), icon=folium.Icon(color="green")))
+
 map.add_child(fg)
 map.save("map1.html")
+
